811694af05ccd78c3bb683866928a8acd965f8bd
sotima <mtieben54@googlemail.com>
2022-03-23 12:33:15 +0100

    Bump revision

--- dist/hass-sotima-icons.js
+++ dist/hass-sotima-icons.js
@@ -46,7 +46,7 @@ if (!window.frontendVersion || window.frontendVersion < 20200519.0) {
 }
 
 console.info(
-  `%c HASS-SOTIMA-ICONS         \n%c Version 1.53 [` + sotima_icons_mode + `] `,
+  `%c HASS-SOTIMA-ICONS         \n%c Version 1.54 [` + sotima_icons_mode + `] `,
   "color: orange; font-weight: bold; background: black",
   "color: white; font-weight: bold; background: dimgray"
 );
